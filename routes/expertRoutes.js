fce24f5815fe25b0dbede9319e443aa5dc6b5eac
Afraz33 <afraz3301@gmail.com>
2023-10-16 16:04:38 +0500

    Change middleware for expert registeration

--- routes/expertRoutes.js
+++ routes/expertRoutes.js
@@ -16,5 +16,29 @@ const ExpertModel = require("../models/expertModel");
 //Expert routes
 expertRoutes.post("/expertSignup", expertSignup, createReview, createCalendar);
 expertRoutes.post("/expert-Login", expertLogin);
+
+expertRoutes.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const experts = await ExpertModel.find({}); // Fetch all experts
+
+    res.json(experts); // Send the experts as a JSON response
+  })
+);
+
+expertRoutes.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const expert = await ExpertModel.findById(req.params.id);
+    if (expert) {
+      res.json(expert);
+    } else {
+      res.status(404);
+      throw new Error("Expert not Found");
+    }
+  })
+);
+
 expertRoutes.put("/expert-updateCalendar", updateCalendar);
+
 module.exports = expertRoutes;
