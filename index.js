e8cb450d88f5c7f0933fdf40964804a88ad04236
umammahh <umamaqasim420@gmail.com>
2023-10-16 14:19:06 +0500

    get reviews , profiles of user , signup and login

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const connectDB = require("./config/database");
 
 connectDB();
 const app = express();
+
 const port = process.env.PORT || 5000;
+
 var cors = require("cors");
 
 app.use(cors());
--- routes/expertRoutes.js
+++ routes/expertRoutes.js
@@ -5,10 +5,32 @@ const {
 } = require("../controllers/expertController");
 const { createReview } = require("../controllers/reviewController");
 const jwt = require("jsonwebtoken");
-
+const asyncHandler = require('express-async-handler');
 const expertRoutes = require("express").Router();
+const ExpertModel = require("../models/expertModel")
 
 //Expert routes
 expertRoutes.post("/expertSignup", expertSignup, createReview);
 expertRoutes.post("/expert-Login", expertLogin);
+expertRoutes.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const experts = await ExpertModel.find({}); // Fetch all experts
+
+    res.json(experts); // Send the experts as a JSON response
+  })
+);
+
+expertRoutes.get("/:id",
+  asyncHandler(async (req, res) => {
+    const expert = await ExpertModel.findById(req.params.id);
+    if (expert) {
+      res.json(expert);
+    } else {
+      res.status(404);
+      throw new Error("Expert not Found");
+    }
+  })
+);
+
 module.exports = expertRoutes;
